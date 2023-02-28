5e13f8e82e9f23593fa88402ad362f2798a4495d
Sean McGill <sean.l.mcgill@gmail.com>
2023-02-27 19:00:08 -0500

    Initial commit

---
+++ apps/nxfun/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxfun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nxfun';
+}
